6e9cf4594f32e35987f627a5294d37a42a2bfdea
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-12 14:48:55 -0300

    finalizando o desafio da troca

--- Desafios/Desafio Da Área de Circuferencia/DesafioDatrocaDeValores.js
+++ Desafios/Desafio Da Área de Circuferencia/DesafioDatrocaDeValores.js
@@ -8,12 +8,17 @@
 */
 let a = 42; // podem ser var, mais nunca const
 let b = 5;
-let temporaria = a
+let temporaria = a;
+console.log('valores original')
 console.log('numero a = ' + a);
 console.log('numero b = ' + b);
 
 console.log('')
+
+console.log('Depois das trocas')
 a = b;
 b = temporaria;
 console.log('numero a = ' + a);
 console.log('numero b = ' + b);
+
+// ou para trocar podemos fazer apenas o seguinte [a , b] = [ b , a]

ddfb3676e5486e5b255f392b3056f124c7f50c92
Edivansanto5 <capic.20191tads0326@aluno.ifpi.edu.br>
2022-03-13 13:22:20 -0300

    finalizando o desfio 05

--- Desafios/Desafios de Fundamentos/areaDeCircuferencia05.js
+++ Desafios/Desafios de Fundamentos/areaDeCircuferencia05.js
@@ -2,8 +2,14 @@
     Planeja-se construir uma piscina circular com uma ilha no meio, também circular. Sabendo que o raio da ilha possui 30 metros e que o raio da piscina possui 50 metros, qual é a área da superfície da piscina? (π = 3,14)
 */
 let PI = 3.14
-let raio1 = 50
-let raio2 = 30
+let raioSuperficePis = 50
+let raioDaIlha = 30
 
-let areaCir1 = PI * Math.pow(raio1,2)
-console.log()
+let areaSuper = PI * Math.pow(raioSuperficePis,2)
+let areaIlha = PI * Math.pow(raioDaIlha,2)
+let areaSuperfTotal = areaSuper - areaIlha
+
+console.log('Área total da surperfice da Piscina é ',areaSuperfTotal + 'm2')
+console.log('Área da surperfice da piscina é ',areaSuper + 'm2')
+console.log('Área da surperfice da ilha da piscina é ',areaIlha + 'm2')
+
